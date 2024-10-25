101d3ab05d04a101439e634f26a3e43856415ea7
minettebrink <mimmi.brink@gmail.com>
2024-10-25 16:06:31 +0200

    prints added

--- main.go
+++ main.go
@@ -3,25 +3,49 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("no website provided")
-		os.Exit(1)
+	if len(os.Args) < 4 {
+		fmt.Println("not enough arguments provided")
+		fmt.Println("usage: crawler <baseURL> <maxConcurrency> <maxPages>")
+		return
 	}
-	if len(os.Args) > 2 {
+	if len(os.Args) > 4 {
 		fmt.Println("too many arguments provided")
-		os.Exit(1)
+		return
 	}
 	rawBaseURL := os.Args[1]
+	maxConcurrencyString := os.Args[2]
+	maxPagesString := os.Args[3]
+
+	maxConcurrency, err := strconv.Atoi(maxConcurrencyString)
+	if err != nil {
+		fmt.Printf("Error - maxConcurrency: %v", err)
+		return
+	}
+	maxPages, err := strconv.Atoi(maxPagesString)
+	if err != nil {
+		fmt.Printf("Error - maxPages: %v", err)
+		return
+	}
+
+	cfg, err := configure(rawBaseURL, maxConcurrency, maxPages)
+	if err != nil {
+		fmt.Printf("Error - configure: %v", err)
+		return
+	}
 
 	fmt.Printf("starting crawl of: %s...\n", rawBaseURL)
 
-	htmlBody, err := getHTML(rawBaseURL)
-	if err != nil {
-		fmt.Println(err)
+	cfg.wg.Add(1)
+	go cfg.crawlPage(rawBaseURL)
+	cfg.wg.Wait()
+
+	for normalizedURL, count := range cfg.pages {
+		fmt.Printf("%d - %s\n", count, normalizedURL)
 	}
 
-	fmt.Print(htmlBody)
+	printReport(cfg.pages, cfg.baseURL.String())
 }
