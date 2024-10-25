b8be4a19c287d7981ca8f87f56b6581bbd5be761
minettebrink <mimmi.brink@gmail.com>
2024-10-25 15:42:33 +0200

    crawling

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-    helloWorld()
-}
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+	if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	rawBaseURL := os.Args[1]
 
-func helloWorld() {
-    fmt.Println("Hello, World!")
+	fmt.Printf("starting crawl of: %s...\n", rawBaseURL)
+
+	htmlBody, err := getHTML(rawBaseURL)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Print(htmlBody)
 }
